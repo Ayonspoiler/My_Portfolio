2b0777cf0a2a405e24d5fbb3d08b3bf0c38bec54
Ayonspoiler <ayon4052@gmail.com>
2025-11-04 15:50:49 +0600

    Updated Stats Page.

--- frontend/components/Stats.jsx
+++ frontend/components/Stats.jsx
@@ -1,48 +1,57 @@
-"use client"
-import CountUp from "react-countup"
+"use client";
+import CountUp from "react-countup";
 
-const stats=[
-    {
-        num:1,
-        text:"Years of Experience" , 
-    },
-    {
-        num:10,
-        text:"Project Completed" ,
-    },
-    {
-        num:8,
-        text:"Technologies Learned" ,
-    },
-    {
-        num:500,
-        text:"Code Commits"
-    }
-]
+const stats = [
+  {
+    num: 1,
+    text: "Years of Experience",
+  },
+  {
+    num: 10,
+    text: "Project Completed",
+  },
+  {
+    num: 8,
+    text: "Technologies Learned",
+  },
+  {
+    num: 500,
+    text: "Code Commits",
+  },
+];
 
 const Stats = () => {
   return (
     <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
-        <div className="container mx-auto">
-            <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
-                {
-                    stats.map((stat, index)=>{
-                        return(
-                            <div key={index} className="flex-1 flex gap-4 items-center justify-center xl:justify-start " >
-                                
-                                    <CountUp end={stat.num} duration={5} delay={2} className="text-4xl xl:text-6xl font-semibold" />
-                                   
-                                <p className={`${stat.text.length<15 ? "max-w-[100px]":"max-w-[150pz]"} leading-snug text-white/80`}>{stat.text}</p>
-                              
-                            </div>
-                        )
+      <div className="container mx-auto">
+        <div className="grid grid-cols-2 gap-6 max-w-[80vw] mx-auto xl:flex xl:max-w-none">
+          {stats.map((stat, index) => {
+            return (
+              <div
+                key={index}
+                className="flex-1 flex gap-4 items-center justify-center xl:justify-start "
+              >
+                <CountUp
+                  end={stat.num}
+                  duration={5}
+                  delay={2}
+                  className="text-4xl xl:text-6xl font-semibold"
+                />
 
-                    })}
-                </div>
-            </div>
-
+                <p
+                  className={`${
+                    stat.text.length < 15 ? "max-w-[100px]" : "max-w-[150pz]"
+                  } leading-snug text-white/80`}
+                >
+                  {stat.text}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Stats
+export default Stats;
