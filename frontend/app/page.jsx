9ea28120012e2237d533000f791a3a3da67f5c3f
Ayonspoiler <ayon4052@gmail.com>
2025-11-04 16:34:04 +0600

    Updated Home Page.

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -27,7 +27,7 @@ const Home = () => {
               <a
                 href="/assets/MY_CV.pdf"
                 download="Syed_Md_Shadman_Alam_CV.pdf"
-                className="uppercase flex items-center gap-2 border border-accent text-accent hover:bg-accent hover:text-primary rounded-full px-6 py-3 transition-all duration-300"
+                className="uppercase flex items-center gap-2 border border-accent text-accent hover:bg-accent hover:text-primary active:bg-accent active:text-primary rounded-full px-6 py-3 transition-all duration-300"
               >
                 <span>Download CV</span>
                 <FiDownload className="text-xl" />
@@ -36,7 +36,7 @@ const Home = () => {
               <div className="mb-8 xl:mb-0">
                 <Socials
                   containerStyles="flex gap-6"
-                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary active:bg-accent active:text-primary hover:transition-all duration-500"
                 />
               </div>
             </div>
